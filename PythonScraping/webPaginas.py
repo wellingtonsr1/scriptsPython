3bfdbae4dcd626544a3f34bfec901c8b73d231f3
wellington <wellington281482@gmail.com>
2022-01-25 21:59:18 -0300

    Alterado os nomes das funções e variáveis

--- PythonScraping/webPaginas.py
+++ PythonScraping/webPaginas.py
@@ -3,56 +3,57 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
-import re
 from tkinter import *
-
-corFundo = "black"
-listaEnderecos = ['link_1', 'Link_n']   
+import re
 
 
-def criarJanela():
-    janela = Tk()
-    janela.title("Login")
-    janela['bg'] = corFundo
+backgroundColor = "black"
+addressList = ['http://helpdesk.ipmjp.pb.gov.br/', 'https://mail.ipmjp.pb.gov.br/']   
 
-    lUsuario = Label(janela, text="Usuário:", font="Arial 12 bold italic", bg=corFundo, fg='white')
-    lSenha = Label(janela, text="Senha:", font="Arial 12 bold italic", bg=corFundo, fg='white')
-    areaUsuario = Entry(janela, width=25)
-    areaSenha = Entry(janela, width=25, show='*')
-    btn = Button(janela, text="Acessar", command=lambda: click(areaUsuario.get(), areaSenha.get()))
 
-    lUsuario.grid(row=0, column=0,pady=20)
-    lSenha.grid(row=1, column=0)
-    areaUsuario.grid(row=0, column=1)
-    areaSenha.grid(row=1, column=1)
+def dislplayLoginWindow():
+    window = Tk()
+    window.title("Login")
+    window['bg'] = backgroundColor
+
+    lUser = Label(window, text="Usuário:", font="Arial 12 bold italic", bg=backgroundColor, fg='white')
+    lPassword = Label(window, text="Senha:", font="Arial 12 bold italic", bg=backgroundColor, fg='white')
+    userLoginField = Entry(window, width=25)
+    passwordLoginField = Entry(window, width=25, show='*')
+    btn = Button(window, text="Acessar", command=lambda: btnClick(userLoginField.get(), passwordLoginField.get()))
+
+    lUser.grid(row=0, column=0,pady=20)
+    lPassword.grid(row=1, column=0)
+    userLoginField.grid(row=0, column=1)
+    passwordLoginField.grid(row=1, column=1)
     btn.grid(row=2, column=0, padx=15, pady=20)
 
-    janela.geometry("245x150+800+400")
-    janela.resizable(width=0, height=0)
-    janela.mainloop()
+    window.geometry("245x150+800+400")
+    window.resizable(width=0, height=0)
+    window.mainloop()
 
-def click(usuario, senha):
+def btnClick(user, password):
     driver = webdriver.Firefox()
 
-    for idx in range(len(listaEnderecos)):
-        if re.search('helpdesk', listaEnderecos[idx]):
-            dadosAcesso = (listaEnderecos[idx], 'login_name', 'login_password', 'submit')
-        elif re.search('mail', listaEnderecos[idx]):
-            dadosAcesso = (listaEnderecos[idx], 'username', 'password', 'ZLoginButton')
-        elif re.search('ramais', listaEnderecos[idx]):
-            dadosAcesso = (listaEnderecos[idx], 'None', 'None', 'None')
-        elif re.search('114', listaEnderecos[idx]):
-            dadosAcesso = (listaEnderecos[idx], 'username', 'password', 'submit')
+    for idx in range(len(addressList)):
+        if re.search('helpdesk', addressList[idx]):
+            accessData = (addressList[idx], 'login_name', 'login_password', 'submit')
+        elif re.search('mail', addressList[idx]):
+            accessData = (addressList[idx], 'username', 'password', 'ZLoginButton')
+        elif re.search('ramais', addressList[idx]):
+            accessData = (addressList[idx], 'None', 'None', 'None')
+        elif re.search('114', addressList[idx]):
+            accessData = (addressList[idx], 'username', 'password', 'submit')
 
         flag = idx
-        abriPagina(dadosAcesso[0], usuario, senha, dadosAcesso[1], dadosAcesso[2], dadosAcesso[3], flag, driver)
+        openWebBrowser(accessData[0], user, password, accessData[1], accessData[2], accessData[3], flag, driver)
 
     print('-' * 30)
     print('*******      Done!     *******')
     print('-' * 30)
     exit(0)
 
-def abriPagina(link, usuario, senha, idLogin, idSenha, classBotao, flag, driver):
+def openWebBrowser(link, user, password, loginId, passowrdId, btnClass, flag, driver):
     if flag != 0:
         driver.execute_script("window.open('');") 
         driver.switch_to.window(driver.window_handles[flag])
@@ -61,21 +62,21 @@ def abriPagina(link, usuario, senha, idLogin, idSenha, classBotao, flag, driver)
     driver.maximize_window() 
     driver.implicitly_wait(20) 
 
-    campoUsuario = driver.find_element(By.ID, idLogin)
-    campoSenha = driver.find_element(By.ID, idSenha)
-    #btn = driver.find_element(By.CLASS_NAME, classBotao)
-    btn = driver.find_element(By.ID, classBotao)
+    userElementField = driver.find_element(By.ID, loginId)
+    passwordElementField = driver.find_element(By.ID, passowrdId)
+    btn = driver.find_element(By.CLASS_NAME, btnClass)
+    #btn = driver.find_element(By.ID, btnClass)
 
     sleep(1)
-    campoUsuario.send_keys(usuario)
+    userElementField.send_keys(user)
     sleep(1.5)
-    campoSenha.send_keys(senha)
+    passwordElementField.send_keys(password)
     sleep(1)
     btn.click()
 
 
 if __name__ == '__main__':
-   criarJanela()
+   dislplayLoginWindow()
 
 
 
