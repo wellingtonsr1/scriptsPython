39f9e6551873b6e8a1e51b13acab730a8b868fc7
wellington <wellington281482@gmail.com>
2022-01-29 01:26:13 -0300

    Algumas variáveis renomeadas e reorganizadas

--- backup/backup.py
+++ backup/backup.py
@@ -17,9 +17,10 @@
 import os, shutil, socket
 from datetime import datetime
 
-root = 'c:\\users'
+root = 'C:\\users'
+drive = 'C:\\'
 currentDate = datetime.today().strftime('%d-%m-%Y')
-pathBackup = 'C:\\bkp_'+socket.gethostname()+'_'+currentDate
+backupFolder = 'bkp_'+socket.gethostname()+'_'+currentDate
 extList = ['.pdf', '.docx', '.xlsx', '.odt', '.ods']
 fileDirectory = 'Arquivos_'
 
@@ -34,7 +35,7 @@ def header():
 def foot():
     print()
     print("-=-" * 30)
-    print('Backup realizado. Verifique os arquivos em '.rjust(50) + pathBackup)  
+    print('Backup realizado. Verifique os arquivos em {}'.rjust(50).format(os.path.join(drive, backupFolder)))
     print()    
 
 def main():  
@@ -42,21 +43,24 @@ def main():
     
     # Lista as pastas dos usuários em 'C:\Users'
     for userFolder in [userFolders for userFolders in os.listdir(root)]:
-        fullPathBackup = os.path.join(pathBackup, userFolder)
+        # C:\\bkp_NomeMaquina_dataBackup\pastaUsusario
+        pathBackupFolder = os.path.join(os.path.join(drive, backupFolder), userFolder)
 
         # Varre a árvore de diretórios a partir de 'C:\Users\pastaUsuário'
         for folder, subfolders, files in os.walk(os.path.join(root, userFolder)):
             for file in files:
                 origin = os.path.join(folder, file)
                 for ext in extList:
+                    # Cria o diretório 'Arquivos_pdf', 'Arquivos_docx' etc
                     fileDirectoryByExt = fileDirectory+ext.replace('.', '')
                     if file.endswith(ext):
-                        backupFolder = os.path.join(fullPathBackup, fileDirectoryByExt)
-                        if not os.path.exists(backupFolder):
-                            os.makedirs(backupFolder)
+                        # Cria 'C:\\bkp_NomeMaquina_dataCriaçãoBackup\pastaUsusário\Arquivos_EXT'
+                        fullBackupFolder = os.path.join(pathBackupFolder, fileDirectoryByExt)
+                        if not os.path.exists(fullBackupFolder):
+                            os.makedirs(fullBackupFolder)
 
-                        destination = os.path.join(backupFolder, file) 
-                        print('Adicionando [%s] na pasta [%s] em [%s]' % (file, fileDirectoryByExt, userFolder))
+                        destination = os.path.join(fullBackupFolder, file) 
+                        print('Adicionando [{}] na pasta [{}] em [{}]'.format(file, fileDirectoryByExt, userFolder))
                         shutil.copy(origin, destination)
 
                     continue
