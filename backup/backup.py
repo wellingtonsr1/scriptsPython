464a6d0710176b1ee6c9db77a181da94f3301556
wellington <wellington281482@gmail.com>
2022-02-01 00:28:02 -0300

    Refeitos alguns nomes de variáeis e funções

--- backup/backup.py
+++ backup/backup.py
@@ -14,44 +14,46 @@
 #+------------------------------------------------------------------------------------------------------------------+
 
 
-import os, shutil, socket
+import os
+import shutil
+import socket
 import re
 from datetime import datetime
 from time import sleep
 
 
 root = 'C:\\users'
-currentDate = datetime.today().strftime('%d-%m-%Y')
-backupFolder = 'bkp_'+socket.gethostname()+'_'+currentDate
-extList = ['.pdf', '.docx', '.xlsx', '.odt', '.ods']
-fileDirectory = 'Arquivos_'
-lenLength = 30
+current_date = datetime.today().strftime('%d-%m-%Y')
+backup_folder = 'bkp_'+socket.gethostname()+'_'+current_date
+ext_list = ['.pdf', '.docx', '.xlsx', '.odt', '.ods']
+file_directory = 'Arquivos_'
+len_length = 30
 
-def header(statusMessage):
-    cleanSceen()
-    print("-=-" * lenLength)
-    print("Status: {}".center(25).format(statusMessage), end='')
+def header(status_message):
+    clean_sceen()
+    print("-=-" * len_length)
+    print("Status: {}".center(25).format(status_message), end='')
     print("Máquina: " + socket.gethostname().ljust(20), end='')
-    print("Data: " + currentDate.ljust(20))
-    print("-=-" * lenLength)
+    print("Data: " + current_date.ljust(20))
+    print("-=-" * len_length)
     print()
 
 def foot():
     print()
-    print("-=-" * lenLength)
+    print("-=-" * len_length)
     print()  
     print('Pressione ENTER pra continuar.')
     os.system('pause > NULL' if os.name == 'nt' else 'continue') 
     raise SystemExit()
 
-def finalInformation(drive):
+def final_information(drive):
     header('Backup realizado')
-    print(' Verifique os arquivos em {}'.rjust(50).format(os.path.join(drive, backupFolder)))
+    print(' Verifique os arquivos em {}'.rjust(50).format(os.path.join(drive, backup_folder)))
 
-def cleanSceen():
+def clean_sceen():
     os.system('cls') 
 
-def getUserFolders():
+def get_user_folders():
     listUserFolders = []
     for userFolder in os.listdir(root):
         if os.path.isdir(os.path.join(root, userFolder)):
@@ -59,7 +61,7 @@ def getUserFolders():
         continue
     return listUserFolders
 
-def getDrive():
+def get_drive():
     drive = re.sub(r'\s+', '', input("Onde deseja salvar os arquivos? (Tecle ENTER para unidade C:\): ".rjust(66)))
     if drive == '':
         drive = 'C'
@@ -71,49 +73,55 @@ def getDrive():
         print('A unidade de disco informada não existe.'.center(95))
         foot()
 
-def processingCore():
-    lineCount = 0  
-    fileCount = 0 
-    drive = getDrive()
+def processing_core():
+    line_count = 0  
+    file_count = 0 
+    drive = get_drive()
     
     header('Backup iniciado')
 
-    for userFolder in getUserFolders():
+    for user_folder in get_user_folders():
         # C:\\bkp_NomeMaquina_dataBackup\pastaUsusario
-        pathBackupFolder = os.path.join(os.path.join(drive, backupFolder), userFolder)
+        path_backup_folder = os.path.join(os.path.join(drive, backup_folder), user_folder)
 
         # Varre a árvore de diretórios a partir de 'C:\Users\pastaUsuário'
-        for folder, subfolders, files in os.walk(os.path.join(root, userFolder)):
-            for file in files:
-                origin = os.path.join(folder, file)
-                for ext in extList:
+        for folder, subfolders, files in os.walk(os.path.join(root, user_folder)):
+
+            for file_name in files:
+                origin = os.path.join(folder, file_name)
+
+                for ext in ext_list:
                     # Cria o diretório 'Arquivos_pdf', 'Arquivos_docx' etc
-                    fileDirectoryByExt = fileDirectory+ext.replace('.', '')
-                    if file.endswith(ext):
-                        fileCount += 1
+                    file_directory_by_ext = file_directory+ext.replace('.', '')
+
+                    if file_name.endswith(ext):
+                        file_count += 1
                         # Cria 'C:\\bkp_NomeMaquina_dataCriaçãoBackup\pastaUsusário\Arquivos_EXT'
-                        fullBackupFolder = os.path.join(pathBackupFolder, fileDirectoryByExt)
-                        if not os.path.exists(fullBackupFolder):
-                            os.makedirs(fullBackupFolder)
+                        full_backup_folder = os.path.join(path_backup_folder, file_directory_by_ext)
+
+                        if not os.path.exists(full_backup_folder):
+                            os.makedirs(full_backup_folder)
                         
-                        destination = os.path.join(fullBackupFolder, file) 
+                        destination = os.path.join(full_backup_folder, file_name) 
                         
                         try:
                             shutil.copy(origin, destination)
-                            if lineCount == 20:
+                            if line_count == 20:
                                 header('Backup iniciado')
-                                lineCount = 0
+                                line_count = 0
 
-                            print('Adicionado [{}] na pasta [{}] em [{}]'.format(file, fileDirectoryByExt, userFolder))
+                            print('Adicionado [{}] na pasta [{}] em [{}]'.format(file_name, file_directory_by_ext, user_folder))
                             sleep(0.3)
-                            lineCount += 1
+                            line_count += 1
                         except Exception as err:
                             print('Error: {}'.format(err))
                             print()
-                            foot()                        
+                            foot()   
+
                     continue
-    if fileCount != 0:
-        finalInformation(drive)
+                
+    if file_count != 0:
+        final_information(drive)
     else:
         header('Backup abortado')
         print('Não há arquivos para copiar.'.center(95))
@@ -121,10 +129,10 @@ def processingCore():
 
 
 def main():  
-    print("-=-" * lenLength)
+    print("-=-" * len_length)
     print('S I S T E M A  D E  B A C K U P'.center(95))
-    print("-=-" * lenLength)
-    processingCore()
+    print("-=-" * len_length)
+    processing_core()
     foot()
 
 
